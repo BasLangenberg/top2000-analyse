7834424ecbef1ef44158355993692b6ef012005a
Bas Langenberg <baslangenberg@gmail.com>
2017-10-21 11:52:27 +0200

    Add skeleton for application

---
+++ application.py
@@ -0,0 +1,7 @@
+import sys
+
+from data import setup
+
+if __name__ == '__main__':
+    setup.prepare_data()
+    sys.exit()
---
+++ data/setup.py
@@ -0,0 +1,10 @@
+import requests
+import openpyxl
+
+def prepare_data():
+    pass
+    # TODO: Get download link for Excel file
+    # TODO: Download Excel file
+    # TODO: Verify Hash for updates
+    # TODO: Import content (Database) if data updated
+
